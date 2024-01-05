5b2ea772027905358fa23064ca51ac2026900e04
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-05 10:55:40 +0900

    init commit
    프로젝트 생성후 초기화업로드

---
+++ src/test/java/com/hobbyhob/HobbyHobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyhob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyHobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
