ff56bf4c9e71696947f1d1b4f6e7c8e72223ab52
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-07 17:42:30 +0900

    feat(#3) : Club 엔티티 작성
    명시적 삭제를 위한 SQLDelete애노테이션 추가 및 기본적인 구조 작성

---
+++ src/main/java/com/hobbyhop/domain/club/entity/Club.java
@@ -0,0 +1,27 @@
+package com.hobbyhop.domain.club.entity;
+
+import com.hobbyhop.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SQLDelete(sql = "UPDATE club SET deleted_at = NOW() where id=?")
+public class Club extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @Column(name="deleted_at")
+    private Timestamp deletedAt;
+
+}
