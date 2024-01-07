a104808c2330b5e4d30418d8be06e96eda149e02
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-07 18:59:33 +0900

    test(#3) : ClubRepositoryTest 작성
    h2 db가 잘 작동하는지 테스트하기 위한 코드 작성

--- src/test/java/com/hobbyhop/domain/club/repository/ClubRepositoryTest.java
+++ src/test/java/com/hobbyhop/domain/club/repository/ClubRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.hobbyhop.domain.club.repository;
 
 import com.hobbyhop.domain.club.entity.Club;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,18 +12,25 @@ import java.util.stream.IntStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Log4j2
 class ClubRepositoryTest {
     @Autowired
     ClubRepository clubRepository;
-
+    @DisplayName("테스트 db 테스트를 위한 메소드")
     @Test
-    void name() {
-        IntStream.rangeClosed(1, 10).forEach(i -> {
-            Club club = Club.builder()
-                    .title("test title" + i)
-                    .content("test content"+i)
-                    .build();
-            clubRepository.save(club);
-        });
+    void givenClubId_whenDoSelectAction_thenReturnsEntity() {
+        // Given
+        Club club = Club.builder()
+                .title("test")
+                .build();
+
+        Club savedClub = clubRepository.save(club);
+
+        Long clubId = savedClub.getId();
+
+        // When
+        Club foundClub = clubRepository.findById(clubId).orElseThrow();
+        // Then
+        log.info(foundClub);
     }
 }
