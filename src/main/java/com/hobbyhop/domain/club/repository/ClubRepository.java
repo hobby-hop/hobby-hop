21e2fb5a34519ef39711202503568b641150b103
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-07 17:42:55 +0900

    feat(#3) : ClubRepository 작성

---
+++ src/main/java/com/hobbyhop/domain/club/repository/ClubRepository.java
@@ -0,0 +1,11 @@
+package com.hobbyhop.domain.club.repository;
+
+import com.hobbyhop.domain.club.entity.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+
+
+}
