fde2d239d3ebc976ff01a88ed11b4aef47ad1a97
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-07 17:41:35 +0900

    feat(#3) : JPA Auditing을 위한 클래스 추가
    JPA의 Auditing을 사용하기 위한 AuditingConfig 설정 클래스 추가, DB메타데이터 createdAt과 modifiedAt 을 자동으로 갱신하도록 하기위한 BaseEntity클래스 추가

---
+++ src/main/java/com/hobbyhop/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.hobbyhop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
+}
