0ee73aa4b6ed7ac7c8f9a1088ae7b7fdde9063af
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-08 17:19:55 +0900

    feat(#7) : ClubServiceImpl클래스 작성
    인터페이스 ClubService의 구현체 클래스 작성. 클래스 레벨에 @Transactional(readOnly=true)를 붙이고 트랜잭션이 필요한 메소드에 트랜잭션을 선언해두어 우선순위에 따라 트랜잭션이 적용되도록 작성.

---
+++ src/main/java/com/hobbyhop/domain/club/service/impl/ClubServiceImpl.java
@@ -0,0 +1,79 @@
+package com.hobbyhop.domain.club.service.impl;
+
+import com.hobbyhop.domain.club.dto.ClubRequestDTO;
+import com.hobbyhop.domain.club.dto.ClubResponseDTO;
+import com.hobbyhop.domain.club.entity.Club;
+import com.hobbyhop.domain.club.repository.ClubRepository;
+import com.hobbyhop.domain.club.service.ClubService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@Transactional(readOnly = true)
+public class ClubServiceImpl implements ClubService {
+    private final ClubRepository clubRepository;
+
+    @Override
+    public List<ClubResponseDTO> getAllList() {
+        List<ClubResponseDTO> list = clubRepository.findAll().stream().map(club ->
+                ClubResponseDTO.builder().title(club.getTitle())
+                        .content(club.getContent())
+                        .createdAt(club.getCreatedAt())
+                        .modifiedAt(club.getModifiedAt())
+                        .build()
+        ).collect(Collectors.toList());
+
+        return list;
+    }
+
+    @Override
+    public ClubResponseDTO getClub(Long clubId) {
+        Club club = clubRepository.findById(clubId).orElseThrow();
+
+        return ClubResponseDTO.fromEntity(club);
+    }
+
+    @Override
+    @Transactional
+    public ClubResponseDTO makeClub(ClubRequestDTO clubRequestDTO) {
+        Club club = Club.builder()
+                .title(clubRequestDTO.getTitle())
+                .content(clubRequestDTO.getContent())
+                .build();
+
+        Club savedClub = clubRepository.save(club);
+
+        return ClubResponseDTO.fromEntity(savedClub);
+    }
+
+    @Override
+    @Transactional
+    public void removeClubById(Long clubId) {
+        clubRepository.deleteById(clubId);
+    }
+
+    @Override
+    @Transactional
+    public ClubResponseDTO modifyClub(Long clubId, ClubRequestDTO clubRequestDTO) {
+        Club club = clubRepository.findById(clubId).orElseThrow();
+
+        if(clubRequestDTO.getTitle() != null) {
+            club.changeTitle(clubRequestDTO.getTitle());
+        }
+
+        if(clubRequestDTO.getContent() != null) {
+            club.changeContent(clubRequestDTO.getContent());
+        }
+
+        Club modifiedClub = clubRepository.save(club);
+
+        return ClubResponseDTO.fromEntity(modifiedClub);
+    }
+}
