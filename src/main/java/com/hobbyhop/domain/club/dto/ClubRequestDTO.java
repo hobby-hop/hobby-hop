92b169ef33eb4610e5ac00d1826f2434972ce286
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-08 17:11:22 +0900

    feat(#7) : ClubRequestDTO 작성
    컨트롤러단에서 서비스단으로 넘겨줄 dto 클래스인 ClubRequestDTO작성. 일단 필드는 단순하게 title(모임 이름)과 content(모임 설명)만으로 구성함 추후 수정가능성 높음

---
+++ src/main/java/com/hobbyhop/domain/club/dto/ClubRequestDTO.java
@@ -0,0 +1,16 @@
+package com.hobbyhop.domain.club.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClubRequestDTO {
+    private String title;
+    private String content;
+}
