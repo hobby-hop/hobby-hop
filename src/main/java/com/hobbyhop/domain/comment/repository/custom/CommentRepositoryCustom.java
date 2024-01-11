02dc2226b33c41eedcb7d0eef17e91e084bcc31b
dearel4416 <81572807+dearel4416@users.noreply.github.com>
2024-01-11 17:12:28 +0900

    feat(#53): comment 좋아요 추가/취소

--- src/main/java/com/hobbyhop/domain/comment/repository/custom/CommentRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.hobbyhop.domain.comment.repository.custom;
-
-public interface CommentRepositoryCustom {
-
-    //List<Comment> findByPostId(Long postId);
-}
--- src/main/java/com/hobbyhop/domain/comment/repository/custom/CommentRepositoryCustomImpl.java
+++
@@ -1,8 +0,0 @@
-package com.hobbyhop.domain.comment.repository.custom;
-
-public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
-//    @Override
-//    public List<Comment> findByPostId(Long postId) {
-//
-//    }
-}
