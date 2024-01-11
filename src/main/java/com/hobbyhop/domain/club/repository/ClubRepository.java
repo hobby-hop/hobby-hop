2a14d024425340cf5562420df370851462a6e78f
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-11 17:03:29 +0900

    feat(#31) : 페이징 처리를 하기위한 설정
    페이징 처리를 하기 위한 club 레포지토리 수정

--- src/main/java/com/hobbyhop/domain/club/repository/ClubRepository.java
+++ src/main/java/com/hobbyhop/domain/club/repository/ClubRepository.java
@@ -1,11 +1,12 @@
 package com.hobbyhop.domain.club.repository;
 
 import com.hobbyhop.domain.club.entity.Club;
+import com.hobbyhop.domain.club.repository.search.ClubSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
-public interface ClubRepository extends JpaRepository<Club, Long> {
+public interface ClubRepository extends JpaRepository<Club, Long>, ClubSearch {
 
 
 }
