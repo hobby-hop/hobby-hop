4ac01064da1b7cb75b8f6946e38121e59622bbdf
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-12 19:07:38 +0900

    feat(#63) : ClubMemberRepositroy 작성
     ClubMemberRepositroy 작성

--- src/main/java/com/hobbyhop/domain/clubmember/repository/ClubMemberRepository.java
+++ src/main/java/com/hobbyhop/domain/clubmember/repository/ClubMemberRepository.java
@@ -3,5 +3,8 @@ package com.hobbyhop.domain.clubmember.repository;
 import com.hobbyhop.domain.clubmember.entity.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+    List<ClubMember> findAllByClub_Id(Long clubId);
 }
