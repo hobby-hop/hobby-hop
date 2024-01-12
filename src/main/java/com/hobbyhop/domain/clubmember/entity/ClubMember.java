f014f6850d1d8b35422da71cd51516129220684b
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-12 14:29:03 +0900

    feat(#63) : club member 엔티티 작성
    club과 user를 참조하도록 작성

---
+++ src/main/java/com/hobbyhop/domain/clubmember/entity/ClubMember.java
@@ -0,0 +1,28 @@
+package com.hobbyhop.domain.clubmember.entity;
+
+import com.hobbyhop.domain.club.entity.Club;
+import jakarta.persistence.*;
+import com.hobbyhop.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClubMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+}
