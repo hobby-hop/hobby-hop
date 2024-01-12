a356dcd09ae56a183b2cbeb042d4af3ad094b109
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-12 11:42:47 +0900

    feat(#64) : Category Controller 작성
    카테고리 등록과 삭제 경로 추가. 추후 admin 권한을 가진 사용자만 등록및 삭제할 수 있도록 수정할 예정

---
+++ src/main/java/com/hobbyhop/domain/category/controller/CategoryRestController.java
@@ -0,0 +1,33 @@
+package com.hobbyhop.domain.category.controller;
+
+import com.hobbyhop.domain.category.dto.CategoryRequestDTO;
+import com.hobbyhop.domain.category.dto.CategoryResponseDTO;
+import com.hobbyhop.domain.category.service.CategoryService;
+import com.hobbyhop.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/category")
+public class CategoryRestController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> makeCategory(@RequestBody CategoryRequestDTO categoryRequestDTO) {
+        CategoryResponseDTO categoryResponseDTO = categoryService.makeCategory(categoryRequestDTO);
+
+        return ResponseEntity.ok(ApiResponse.ok(categoryResponseDTO));
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<ApiResponse> removeCategory(@PathVariable("categoryId") Long categoryId) {
+        categoryService.removeCategory(categoryId);
+
+        return ResponseEntity.ok(ApiResponse.ok("카테고리가 삭제되었습니다."));
+    }
+
+
+}
