0e799631ea0f191c29b5532581dbfc5d1ec87057
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-12 21:55:34 +0900

    feat (#24) : Refresh 토큰 정책 구현

--- src/main/java/com/hobbyhop/global/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/hobbyhop/global/security/filter/JwtAuthorizationFilter.java
@@ -27,12 +27,36 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String tokenValue = jwtUtil.getJwtFromHeader(request);
+        String accessToken = jwtUtil.getJwtFromHeader(request);
 
-        if (Objects.nonNull(tokenValue)) {
-            if (!jwtUtil.validateToken(tokenValue)) {
-//                ObjectMapper objectMapper = new ObjectMapper();
+        if (Objects.nonNull(accessToken)) {
 
+            // 로그아웃이 되었는지 확인
+            if (jwtUtil.checkIsLoggedOut(accessToken)) {
+                accessToken = jwtUtil.createExpiredToken(accessToken);
+            }
+
+            String accessTokenValue = accessToken.substring(7);
+
+            // accessToken이 만료되었는지 확인
+            if (jwtUtil.shouldAccessTokenBeRefreshed(accessToken.substring(7))) {
+                String refreshTokenValue = jwtUtil.getRefreshtokenByAccessToken(accessToken).substring(7);
+                // refreshtoken이 유효한지 확인
+                if (jwtUtil.validateToken(refreshTokenValue)) {
+                    // accessToken 재발급
+                    String newAccessToken = jwtUtil.createAccessTokenByRefreshToken(refreshTokenValue);
+                    response.setHeader(JwtUtil.AUTHORIZATION_HEADER, newAccessToken);
+
+                    // DB 토큰도 새로고침
+                    jwtUtil.regenerateToken(newAccessToken, accessToken, refreshTokenValue);
+
+                    // 재발급된 토큰으로 검증 진행하도록 대입
+                    accessTokenValue = newAccessToken.substring(7);
+                }
+                // refreshToken이 유효하지 않다면 accessToken 재발급 없이 만료된 상태로 진행
+            }
+
+            if (!jwtUtil.validateToken(accessTokenValue)) {
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 response.setContentType("application/json");
                 response.setCharacterEncoding("UTF-8");
@@ -41,7 +65,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 return;
             }
 
-            Claims info = jwtUtil.getUserInfo(tokenValue);
+            Claims info = jwtUtil.getUserInfo(accessTokenValue);
 
             String username = info.getSubject();
             setAuthentication(username);
@@ -49,31 +73,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        String tokenValue = jwtUtil.getJwtFromHeader(request);
-//
-//        if(StringUtils.hasText(tokenValue)) {
-//            if(!jwtUtil.validateToken(tokenValue)) {
-//                ObjectMapper objectMapper = new ObjectMapper();
-//
-//                response.setStatus(400);
-//                response.setContentType("application/json");
-//                response.setCharacterEncoding("UTF-8");
-//                response.sendError(400, "유효한 토큰이 아닙니다.");
-//
-//                return;
-//            }
-//
-//            Claims info = jwtUtil.getUserInfo(tokenValue);
-//
-//            String userName = info.getSubject();
-//            setAuthentication(userName);
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
 
     public void setAuthentication(String username) {
 
