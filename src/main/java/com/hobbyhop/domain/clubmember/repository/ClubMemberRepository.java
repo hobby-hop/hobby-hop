989666a41498e1a640aaea83338e5aeb70ae54f1
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-13 11:57:26 +0900

    feat(#63) : ClubMemberRepo
    ClubMemberRepo를 생성하고 쿼리메소드 작성.
    해당 클럽에 조회하려는 유저가 있는지 확인하고 있다면 리턴해준다.

--- src/main/java/com/hobbyhop/domain/clubmember/repository/ClubMemberRepository.java
+++ src/main/java/com/hobbyhop/domain/clubmember/repository/ClubMemberRepository.java
@@ -4,7 +4,10 @@ import com.hobbyhop.domain.clubmember.entity.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
     List<ClubMember> findAllByClub_Id(Long clubId);
+
+    Optional<ClubMember> findByClub_IdAndUser_Id(Long clubId, Long userId);
 }
