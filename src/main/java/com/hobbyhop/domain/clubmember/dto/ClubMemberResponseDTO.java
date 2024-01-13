7d7721fc68a06dc34de555edaeca2f314f13ca05
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-13 12:04:44 +0900

    feat(#63) : 컨트롤러에서 리턴해줄 클럽 멤버dto 작성
    클럽 멤버 리스폰스 dto 작성

---
+++ src/main/java/com/hobbyhop/domain/clubmember/dto/ClubMemberResponseDTO.java
@@ -0,0 +1,25 @@
+package com.hobbyhop.domain.clubmember.dto;
+
+import com.hobbyhop.domain.clubmember.entity.ClubMember;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClubMemberResponseDTO {
+    private Long id;
+    private Long userId;
+    private Long clubId;
+
+    public static ClubMemberResponseDTO fromEntity(ClubMember clubMember) {
+        return ClubMemberResponseDTO.builder()
+                .userId(clubMember.getUser().getId())
+                .clubId(clubMember.getClub().getId())
+                .build();
+
+    }
+}
