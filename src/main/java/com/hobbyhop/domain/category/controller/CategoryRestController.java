6c1af3c1e137f47366b6fe867aa98f2428dd2ebe
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-13 18:21:43 +0900

    feat(#82) : 인증이 필요한 컨트롤러에 보안헤더 설정.
    Swagger인증이 필요한 Category, Club컨트롤러에 보안헤더 설정해줌. 모두 인증이 필요한 경로라 클래스 레벨의 어노테이션 붙임.

--- src/main/java/com/hobbyhop/domain/category/controller/CategoryRestController.java
+++ src/main/java/com/hobbyhop/domain/category/controller/CategoryRestController.java
@@ -4,6 +4,7 @@ import com.hobbyhop.domain.category.dto.CategoryRequestDTO;
 import com.hobbyhop.domain.category.dto.CategoryResponseDTO;
 import com.hobbyhop.domain.category.service.CategoryService;
 import com.hobbyhop.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/category")
+@SecurityRequirement(name = "Bearer Authentication")
 public class CategoryRestController {
 
     private final CategoryService categoryService;
--- src/main/java/com/hobbyhop/domain/club/controller/ClubRestController.java
+++ src/main/java/com/hobbyhop/domain/club/controller/ClubRestController.java
@@ -7,17 +7,18 @@ import com.hobbyhop.global.request.PageRequestDTO;
 import com.hobbyhop.global.response.ApiResponse;
 import com.hobbyhop.global.response.PageResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/api/clubs")
+@SecurityRequirement(name = "Bearer Authentication")
 public class ClubRestController {
     private final ClubService clubService;
 
