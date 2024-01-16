ff829ab91c75872f44f83fdda341b5536c215276
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-16 20:30:29 +0900

    fix (#76) : 회원 정보 변경 API 로직 수정(비밀번호 변경 시 평문으로 저장되는 문제 해결)

--- src/main/java/com/hobbyhop/domain/user/entity/User.java
+++ src/main/java/com/hobbyhop/domain/user/entity/User.java
@@ -1,14 +1,7 @@
 package com.hobbyhop.domain.user.entity;
 
 import com.hobbyhop.domain.user.constant.UserRoleEnum;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,57 +15,57 @@ import static com.hobbyhop.domain.user.constant.UserRoleEnum.USER;
 @NoArgsConstructor
 @Table(name = "users")
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(length = 50, nullable = false, unique = true)
-	private String username;
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
 
-	@Column(length = 50, nullable = false, unique = true)
-	private String email;
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
 
-	@Column(length = 100, nullable = false)
-	private String password;
+    @Column(length = 100, nullable = false)
+    private String password;
 
-//	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private UserRoleEnum role = USER;
+    //	@Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role = USER;
 
-	private Long kakaoId;
+    private Long kakaoId;
 
-	@Builder
-	public User(String username, String email, String password) {
-		this.username = username;
-		this.email = email;
-		this.password = password;
-		this.role = USER;
-	}
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = USER;
+    }
 
-	public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.role = role;
-		this.kakaoId =kakaoId;
-	}
+    public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = kakaoId;
+    }
 
-	public void updateProfile(String updateUsername, String updateEmail, String updatePassword) {
-		if (updateUsername != null && !updateUsername.isEmpty()) {
-			this.username = updateUsername;
-		}
+    public void updateProfile(String updateUsername, String updateEmail, String updatePassword) {
+        if (updateUsername != null && !updateUsername.isEmpty()) {
+            this.username = updateUsername;
+        }
 
-		if (updateEmail != null && !updateEmail.isEmpty()) {
-			this.email = updateEmail;
-		}
+        if (updateEmail != null && !updateEmail.isEmpty()) {
+            this.email = updateEmail;
+        }
 
-		if (updatePassword != null && !updatePassword.isEmpty()) {
-			this.password = updatePassword;
-		}
-	}
+        if (updatePassword != null && !updatePassword.isEmpty()) {
+            this.password = updatePassword;
+        }
+    }
 
-	public User kakaoIdUpdate(Long kakaoId) {
-		this.kakaoId = kakaoId;
-		return this;
-	}
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
+    }
 }
--- src/main/java/com/hobbyhop/domain/user/service/UserService.java
+++ src/main/java/com/hobbyhop/domain/user/service/UserService.java
@@ -1,12 +1,12 @@
 package com.hobbyhop.domain.user.service;
 
-import com.hobbyhop.domain.user.dto.UpdateProfileDTO;
-import com.hobbyhop.global.security.jwt.JwtUtil;
-import com.hobbyhop.global.security.userdetails.UserDetailsImpl;
 import com.hobbyhop.domain.user.dto.LoginRequestDTO;
 import com.hobbyhop.domain.user.dto.SignupRequestDTO;
+import com.hobbyhop.domain.user.dto.UpdateProfileDTO;
 import com.hobbyhop.domain.user.entity.User;
 import com.hobbyhop.domain.user.repository.UserRepository;
+import com.hobbyhop.global.security.jwt.JwtUtil;
+import com.hobbyhop.global.security.userdetails.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -36,6 +36,7 @@ public class UserService {
                 .build();
         userRepository.save(user);
     }
+
     public void login(LoginRequestDTO loginRequestDTO, HttpServletResponse response) {
         String email = loginRequestDTO.getEmail();
         String password = loginRequestDTO.getPassword();
@@ -84,9 +85,10 @@ public class UserService {
 
         validatePassword(user, updateProfileDTO.getOldPassword());
         editComparison(updateProfileDTO);
+        String newPassword = passwordEncoder.encode(updateProfileDTO.getNewPassword());
 
         // Call the updateProfile method in the User entity
-        user.updateProfile(updateProfileDTO.getUsername(), updateProfileDTO.getEmail(), updateProfileDTO.getConfirmPassword());
+        user.updateProfile(updateProfileDTO.getUsername(), updateProfileDTO.getEmail(), newPassword);
 
         // token의 subject를 username으로 발급했기 때문에
         // token을 바뀐 username으로 재발급
@@ -124,16 +126,16 @@ public class UserService {
         }
     }
 
-    private void validateExistingUser (SignupRequestDTO signupRequestDTO) {
-        if(userRepository.findByUsername((signupRequestDTO.getUsername())).isPresent()){
+    private void validateExistingUser(SignupRequestDTO signupRequestDTO) {
+        if (userRepository.findByUsername((signupRequestDTO.getUsername())).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 유저입니다.");
         }
 
-        if(userRepository.findByEmail((signupRequestDTO.getEmail())).isPresent()){
+        if (userRepository.findByEmail((signupRequestDTO.getEmail())).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
 
-        if (! signupRequestDTO.getConfirmPassword().equals(signupRequestDTO.getPassword())) {
+        if (!signupRequestDTO.getConfirmPassword().equals(signupRequestDTO.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
     }
