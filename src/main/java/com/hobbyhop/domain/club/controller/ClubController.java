06b37450b06cb2cc9b339c9623c9b977f0abd8bf
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-16 16:22:42 +0900

    refactor(#109) : 컨트롤러 리턴타입 수정
    ResponseEntity에서 컨벤션에 맞게 ApiResponse 타입으로 수정

--- src/main/java/com/hobbyhop/domain/club/controller/ClubController.java
+++ src/main/java/com/hobbyhop/domain/club/controller/ClubController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,24 +28,24 @@ public class ClubController {
 
     @Operation(summary = "새로운 모임 생성")
     @PostMapping
-    public ResponseEntity<ApiResponse> makeClub(@RequestBody ClubRequestDTO clubRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ApiResponse<?> makeClub(@RequestBody ClubRequestDTO clubRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         ClubResponseDTO clubResponseDTO = clubService.makeClub(clubRequestDTO, userDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.ok(clubResponseDTO));
+        return ApiResponse.ok(clubResponseDTO);
     }
 
     // TODO : implement 검색, 카테고리별 정렬
     @Operation(summary = "모든 모임 리스트 조회")
     @GetMapping
-    public ResponseEntity<ApiResponse> getClubList(PageRequestDTO pageRequestDTO) {
+    public ApiResponse<?> getClubList(PageRequestDTO pageRequestDTO) {
         PageResponseDTO<ClubResponseDTO> list = clubService.getAllClubs(pageRequestDTO);
-        return ResponseEntity.ok(ApiResponse.ok(list));
+        return ApiResponse.ok(list);
     }
 
     @Operation(summary = "모임 조회")
     @GetMapping("/{clubId}")
-    public ResponseEntity<ApiResponse> getClub(@PathVariable("clubId") Long clubId) {
+    public ApiResponse<?> getClub(@PathVariable("clubId") Long clubId) {
         ClubResponseDTO clubResponseDTO = clubService.getClub(clubId);
-        return ResponseEntity.ok(ApiResponse.ok(clubResponseDTO));
+        return ApiResponse.ok(clubResponseDTO);
     }
 
     @Operation(summary = "내가 가입한 모임 조회")
@@ -58,22 +57,22 @@ public class ClubController {
 
     @Operation(summary = "모임 정보 수정")
     @PatchMapping("/{clubId}")
-    public ResponseEntity<ApiResponse> modifyClub(@PathVariable("clubId") Long clubId, @RequestBody ClubRequestDTO clubRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ApiResponse<?> modifyClub(@PathVariable("clubId") Long clubId, @RequestBody ClubRequestDTO clubRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         ClubResponseDTO clubResponseDTO = clubService.modifyClub(clubId, clubRequestDTO, userDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.ok(clubResponseDTO));
+        return ApiResponse.ok(clubResponseDTO);
     }
 
     @Operation(summary = "모임 삭제")
     @DeleteMapping("/{clubId}")
-    public ResponseEntity<ApiResponse> removeClub(@PathVariable("clubId") Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ApiResponse<?> removeClub(@PathVariable("clubId") Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         clubService.removeClubById(clubId, userDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.ok("삭제 성공"));
+        return ApiResponse.ok("삭제 성공");
     }
 
     @Operation(summary = "모임 탈퇴")
     @DeleteMapping("/{clubId}/clubmembers")
-    public ResponseEntity<ApiResponse> leaveClub(@PathVariable("clubId") Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ApiResponse<?> leaveClub(@PathVariable("clubId") Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         clubMemberService.removeMember(clubId, userDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.ok("탈퇴 완료"));
+        return ApiResponse.ok("탈퇴 완료");
     }
 }
