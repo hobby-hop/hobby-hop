8d44d09eaee880c3314b61a235964691c28a8d14
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-16 14:21:44 +0900

    refactor(#103) : 가입 신청 소프트 딜리트 적용
    가입 신청 소프트 딜리트 적용

--- src/main/java/com/hobbyhop/domain/joinrequest/entity/JoinRequest.java
+++ src/main/java/com/hobbyhop/domain/joinrequest/entity/JoinRequest.java
@@ -9,6 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
 
 //유저가 모임에 초대를 누르면 리퀘스트 테이블에 기록된다.
 //유저쪽과 연계해서 알람 목록에서 허락을 누르면 그 때 멤버 테이블에 기록한다.
@@ -17,6 +21,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Getter
+@SQLDelete(sql = "UPDATE join_request SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
 public class JoinRequest extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +35,9 @@ public class JoinRequest extends BaseEntity {
     private Club club;
     @Enumerated(EnumType.STRING)
     private JoinRequestStatus status;
+
+    @Column(name="deleted_at")
+    private Timestamp deletedAt;
 
     public void changeStatus(JoinRequestStatus status) {
         this.status = status;
