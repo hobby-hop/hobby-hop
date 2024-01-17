2a1eaa8b97ee945f9cb3cfe8dff45dfa8b577448
dearel4416 <81572807+dearel4416@users.noreply.github.com>
2024-01-17 16:41:36 +0900

    refactor(#119): service - repository 단일화

--- src/main/java/com/hobbyhop/domain/category/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/hobbyhop/domain/category/service/impl/CategoryServiceImpl.java
@@ -6,7 +6,6 @@ import com.hobbyhop.domain.category.entity.Category;
 import com.hobbyhop.domain.category.repository.CategoryRepository;
 import com.hobbyhop.domain.category.service.CategoryService;
 import com.hobbyhop.global.exception.category.CategoryNotFoundException;
-import com.hobbyhop.global.exception.post.PostNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
