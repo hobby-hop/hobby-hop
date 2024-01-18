58b91ce681dfa1c32cb6d14f916408477f77fdae
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-19 01:35:56 +0900

    feat(144) : categoryController 수정
    유효성 검사를 하도록 설정하고 애노테이션을 붙여서 스웨거에 나타날 경로에 대한 설명을 추가함.

--- src/main/java/com/hobbyhop/domain/category/controller/CategoryController.java
+++ src/main/java/com/hobbyhop/domain/category/controller/CategoryController.java
@@ -1,10 +1,11 @@
 package com.hobbyhop.domain.category.controller;
 
 import com.hobbyhop.domain.category.dto.CategoryRequestDTO;
-import com.hobbyhop.domain.category.dto.CategoryResponseDTO;
 import com.hobbyhop.domain.category.service.CategoryService;
 import com.hobbyhop.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +17,13 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "카테고리 생성")
     @PostMapping
-    public ApiResponse<?> makeCategory(@RequestBody CategoryRequestDTO categoryRequestDTO) {
-
+    public ApiResponse<?> makeCategory(@Valid @RequestBody CategoryRequestDTO categoryRequestDTO) {
         return ApiResponse.ok(categoryService.makeCategory(categoryRequestDTO));
     }
 
+    @Operation(summary = "카테고리 삭제")
     @DeleteMapping("/{categoryId}")
     public ApiResponse<?> removeCategory(@PathVariable("categoryId") Long categoryId) {
         categoryService.removeCategory(categoryId);
