8a0c371c744f02cfcd7ef9f3134927406b45d45f
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-21 16:04:56 +0900

    fix(#155) : 카테고리명 중복확인을 위해 쿼리메소드 선언
    카테고리명 중복확인을 위해 쿼리메소드 선언

--- src/main/java/com/hobbyhop/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/hobbyhop/domain/category/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.hobbyhop.domain.category.repository;
 import com.hobbyhop.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.Optional;
 
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategoryName(String categoryName);
 }
