6d8250a95905fe77fef1d9e6d6802882b064f867
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-23 22:17:27 +0900

    test(#140) : club 비즈니스로직 테스트코드 작성

--- src/test/java/com/hobbyhop/domain/club/service/impl/ClubServiceImplTest.java
+++ src/test/java/com/hobbyhop/domain/club/service/impl/ClubServiceImplTest.java
@@ -1,10 +1,10 @@
-package com.hobbyhop.domain.club.service;
+package com.hobbyhop.domain.club.service.impl;
 
 import com.hobbyhop.domain.category.service.impl.CategoryServiceImpl;
 import com.hobbyhop.domain.club.dto.ClubRequestDTO;
 import com.hobbyhop.domain.club.dto.ClubResponseDTO;
 import com.hobbyhop.domain.club.repository.ClubRepository;
-import com.hobbyhop.domain.club.service.impl.ClubServiceImpl;
+import com.hobbyhop.domain.club.service.impl.impl.ClubServiceImpl;
 import com.hobbyhop.domain.clubmember.entity.ClubMember;
 import com.hobbyhop.domain.clubmember.enums.MemberRole;
 import com.hobbyhop.domain.clubmember.pk.ClubMemberPK;
@@ -25,12 +25,13 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.verify;
 
 
 @DisplayName("[Club]")
 @ExtendWith(MockitoExtension.class)
 @Log4j2
-class ClubServiceTest implements ClubTest {
+class ClubServiceImplTest implements ClubTest {
 
     @InjectMocks
     private ClubServiceImpl sut;
@@ -43,6 +44,7 @@ class ClubServiceTest implements ClubTest {
     private ClubRequestDTO clubRequestDTO;
     private ClubMember clubMember;
     private ClubMemberPK clubMemberPK;
+    private ClubResponseDTO clubResponseDTO;
     private ClubRequestDTO otherClubRequestDTO;
 
     @BeforeEach
@@ -63,12 +65,24 @@ class ClubServiceTest implements ClubTest {
                 .categoryId(TEST_CATEGORY_ID)
                 .build();
 
+        clubResponseDTO = ClubResponseDTO.fromEntity(TEST_CLUB);
+
         otherClubRequestDTO = clubRequestDTO.builder()
                 .title(TEST_OTHER_CLUB_TITLE)
                 .content(TEST_OTHER_CLUB_CONTENT)
                 .categoryId(TEST_OTHER_CATEGORY_ID)
                 .build();
 
+    }
+
+    @DisplayName("[GetClub]")
+    @Test
+    void club_단일_조회() {
+        given(clubRepository.findById(TEST_CLUB_ID)).willReturn(Optional.of(TEST_CLUB));
+
+        assertThat(sut.findClub(TEST_CLUB_ID).getId()).isEqualTo(clubResponseDTO.getId());
+        assertThat(sut.findClub(TEST_CLUB_ID).getTitle()).isEqualTo(clubResponseDTO.getTitle());
+        assertThat(sut.findClub(TEST_CLUB_ID).getContent()).isEqualTo(clubResponseDTO.getContent());
     }
 
     @DisplayName("[Make]")
@@ -78,6 +92,7 @@ class ClubServiceTest implements ClubTest {
         given(categoryService.findCategory(TEST_CATEGORY_ID)).willReturn(TEST_CATEGORY);
         given(clubRepository.save(any())).willReturn(TEST_CLUB);
         // When & Then
+//        verify(clubMemberService.);
         assertThat(sut.makeClub(clubRequestDTO, TEST_USER).getTitle()).isEqualTo(TEST_CLUB_TITLE);
         assertThat(sut.makeClub(clubRequestDTO, TEST_USER).getContent()).isEqualTo(TEST_CLUB_CONTENT);
     }
@@ -112,5 +127,10 @@ class ClubServiceTest implements ClubTest {
 
         // When & Then
         assertThatCode(() -> sut.removeClubById(TEST_CLUB_ID, TEST_USER)).doesNotThrowAnyException();
+    }
+    @DisplayName("[GetMyClubs]")
+    @Test
+    void club_내가_속한_클럽_리스트_조회() {
+
     }
 }
