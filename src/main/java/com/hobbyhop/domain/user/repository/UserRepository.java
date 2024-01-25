73e38640566062453ba7aab04dc10b6d4fe4957c
wkdehf217 <wkdehfdl17@naver.com>
2024-01-25 20:52:54 +0900

    refactor(#211) : UserRepository exist 메서드를 querydsl로 변경

--- src/main/java/com/hobbyhop/domain/user/repository/UserRepository.java
+++ src/main/java/com/hobbyhop/domain/user/repository/UserRepository.java
@@ -1,13 +1,15 @@
 package com.hobbyhop.domain.user.repository;
 
+import com.hobbyhop.domain.user.repository.custom.UserRepositoryCustom;
 import java.util.Optional;
 import com.hobbyhop.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByUsername(String username);
-	Optional<User> findByEmail(String email);
-	boolean existsByUsername(String username);
-	boolean existsByEmail(String email);
-	Optional<User> findByKakaoId(Long kakaoId);
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByKakaoId(Long kakaoId);
 }
