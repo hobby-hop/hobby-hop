1c6cc259d2ae96be0476533b1357a40e6ed36a3e
dearel4416 <81572807+dearel4416@users.noreply.github.com>
2024-01-25 16:20:08 +0900

    fix(#205): reduce club, post delete query connection

--- src/main/java/com/hobbyhop/domain/post/repository/custom/PostRepositoryCustomImpl.java
+++ src/main/java/com/hobbyhop/domain/post/repository/custom/PostRepositoryCustomImpl.java
@@ -7,7 +7,6 @@ import static com.hobbyhop.domain.user.entity.QUser.user;
 
 import com.hobbyhop.domain.post.dto.PostPageResponseDTO;
 import com.hobbyhop.domain.post.dto.PostResponseDTO;
-import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
