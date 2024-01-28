ac56420c3b8cf209ab6f2af7efca94cc5a78cec3
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-28 18:10:46 +0900

    refactor(#240) : ClubRequestDTO 수정
    프론트 레이아웃을 고려해 유효성 검사 조건 수정.  모임 명은 3자 이상 30자 이하, 모임 소개는 3자 이상, 255자 이하로 설정

--- src/main/java/com/hobbyhop/domain/club/dto/ClubRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/club/dto/ClubRequestDTO.java
@@ -14,11 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ClubRequestDTO {
-    @NotBlank(message = "모임 명을 입력해주세요")
-    @Size(min = 1, max = 25)
+    @NotBlank(message = "모임 명을 입력해주세요.")
+    @Size(min = 3, max = 30, message = "모임 명은 3자 이상 30자 이하로 입력해주세요.")
     private String title;
-    @NotBlank(message = "모임 소개를 입력해주세요")
+    @NotBlank(message = "모임 소개를 입력해주세요.")
+    @Size(min = 3, max = 255, message = "모임 소개는 3자 이상 255자 이하로 입력해주세요.")
     private String content;
-    @NotNull(message = "모임의 카테고리를 선택해주세요")
+    @NotNull(message = "모임의 카테고리를 선택해주세요.")
     private Long categoryId;
 }
