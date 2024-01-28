81c53b2c2d90ce80fbc1b444fbd6321952d6b1a8
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-28 18:20:46 +0900

    refactor(#240) : 카테고리 아이디 유효성 검사 조건 추가
    카테고리 번호는 최소 1이상이므로 1 이하일 경우 예외를 반환하도록 유효성 검사 조건 추가. 스웨거 테스트 완료

--- src/main/java/com/hobbyhop/domain/club/dto/ClubModifyDTO.java
+++ src/main/java/com/hobbyhop/domain/club/dto/ClubModifyDTO.java
@@ -1,6 +1,6 @@
 package com.hobbyhop.domain.club.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,5 +16,6 @@ public class ClubModifyDTO {
     private String title;
     @Size(min = 3, max = 255, message = "모임 소개는 3자 이상 255자 이하로 입력해주세요")
     private String content;
+    @Min(value = 1, message = "categoryId는 1 이상이어야 합니다.")
     private Long categoryId;
 }
