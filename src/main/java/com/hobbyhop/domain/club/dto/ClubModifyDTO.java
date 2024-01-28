a0000f7007da95cec66c013fe359cc24dadeb76a
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-28 18:08:26 +0900

    refactor(#240) : club에 길이제한 유효성 조건 수정
    ClubModifyDTO 수정. 프론트 레이아웃을 고려해 유효성검사조건 수정. 모임 이름은 3자 이상 30자 이하로 설정하고 모임 설명은 3자 이상 255자 이하로 설정

--- src/main/java/com/hobbyhop/domain/club/dto/ClubModifyDTO.java
+++ src/main/java/com/hobbyhop/domain/club/dto/ClubModifyDTO.java
@@ -1,5 +1,6 @@
 package com.hobbyhop.domain.club.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,9 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClubModifyDTO {
-
-    @Size(min = 3, message = "최소 3자 이상이여야 합니다.")
+    @Size(message = "모임 명은 3자 이상 30자 이하로 입력해주세요")
     private String title;
+    @Size(min = 3, max = 255, message = "모임 소개는 3자 이상 255자 이하로 입력해주세요")
     private String content;
     private Long categoryId;
 }
