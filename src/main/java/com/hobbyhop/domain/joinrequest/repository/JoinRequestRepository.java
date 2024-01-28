ebb0c405dbbb284d3ca04f7afe6b45e27fc4d0e4
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-29 02:01:19 +0900

    feat(#242) : 쿼리dsl을 사용하기 위한 변경사항

--- src/main/java/com/hobbyhop/domain/joinrequest/repository/JoinRequestRepository.java
+++ src/main/java/com/hobbyhop/domain/joinrequest/repository/JoinRequestRepository.java
@@ -2,11 +2,13 @@ package com.hobbyhop.domain.joinrequest.repository;
 
 import com.hobbyhop.domain.joinrequest.entity.JoinRequest;
 import com.hobbyhop.domain.joinrequest.enums.JoinRequestStatus;
+import com.hobbyhop.domain.joinrequest.repository.custom.JoinRequestRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface JoinRequestRepository extends JpaRepository<JoinRequest, Long> {
+public interface JoinRequestRepository extends JpaRepository<JoinRequest, Long>, JoinRequestRepositoryCustom {
 
     List<JoinRequest> findByClub_IdAndStatus(Long clubId, JoinRequestStatus status);
+
 }
