9826e74ed32a1e67fb0224afb7df9694771e31d8
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-29 13:37:33 +0900

    feature(#237) : LoginRequestDTO - @NotBlank 삭제

--- src/main/java/com/hobbyhop/domain/user/dto/LoginRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/user/dto/LoginRequestDTO.java
@@ -1,13 +1,9 @@
 package com.hobbyhop.domain.user.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
-	@NotBlank(message = "이메일을 입력해주세요.")
 	private String email;
-
-	@NotBlank(message = "비밀번호를 입력해주세요.")
 	private String password;
 }
