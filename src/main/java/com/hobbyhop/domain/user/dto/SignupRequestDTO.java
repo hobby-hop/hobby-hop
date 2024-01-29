fb3490c94b5155eff442cb779444c693bc6e1015
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-29 22:13:25 +0900

    feature(#237) : SignupRequestDTO - info 필드 추가

--- src/main/java/com/hobbyhop/domain/user/dto/SignupRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/user/dto/SignupRequestDTO.java
@@ -23,6 +23,11 @@ public class SignupRequestDTO {
 			max = 50)
 	private String email;
 
+	@NotBlank(message = "info : 자기소개를 3~50자 입력해주세요.")
+	@Size(	min = 3,
+			max = 50)
+	private String info;
+
 	@NotBlank(message = "password : 비밀번호를 입력해주세요.")
 	@Pattern(regexp = "^[a-zA-Z0-9]+$",
 			 message = "password : 비밀번호는 알파벳 대소문자, 숫자의 조합으로 입력해야 합니다.")
