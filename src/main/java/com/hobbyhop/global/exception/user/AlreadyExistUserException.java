81b0029082256a6e0fc9177f3dd6b2481d7302bc
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-29 15:02:18 +0900

    feature(#237) : Exception - 회원 탈퇴 예외 처리 추가

--- src/main/java/com/hobbyhop/global/exception/user/AlreadyExistUserException.java
+++ src/main/java/com/hobbyhop/global/exception/user/AlreadyExistUserException.java
@@ -3,8 +3,9 @@ package com.hobbyhop.global.exception.user;
 import com.hobbyhop.global.exception.common.BusinessException;
 import com.hobbyhop.global.exception.common.ErrorCode;
 
-public class NotAvailableUsername extends BusinessException {
-    public NotAvailableUsername() {
-        super(ErrorCode.NOT_AVAILABLE_USERNAME);
+public class AlreadyExistUserException extends BusinessException {
+
+    public AlreadyExistUserException() {
+        super(ErrorCode.ALREADY_EXIST_USER_EXCEPTION);
     }
 }
