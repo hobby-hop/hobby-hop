6a686ea3044de64ce410c9f857dd176edbbfc422
kkamjjing2 <kkamjjing2@gmail.com>
2024-01-29 23:19:47 +0900

    feature(#237) : OtherProfileResponseDTO - 다른 유저 프로필 조회 시 info 필드 조회 추가

--- src/main/java/com/hobbyhop/domain/user/dto/OtherProfileResponseDTO.java
+++ src/main/java/com/hobbyhop/domain/user/dto/OtherProfileResponseDTO.java
@@ -8,10 +8,12 @@ import lombok.Getter;
 @Builder
 public class OtherProfileResponseDTO {
     private String username;
+    private String info;
 
     public static OtherProfileResponseDTO fromEntity (User user) {
         return OtherProfileResponseDTO.builder()
                 .username(user.getUsername())
+                .info(user.getInfo())
                 .build();
     }
 }
