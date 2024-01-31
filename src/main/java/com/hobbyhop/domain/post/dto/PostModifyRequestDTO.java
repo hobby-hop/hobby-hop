e706d47dfdd1736ee453c4fa9394a884df95649f
wkdehf217 <wkdehfdl17@naver.com>
2024-01-31 11:06:38 +0900

    refactor(#268) : 게시글 title과 content 글자 수 제한

--- src/main/java/com/hobbyhop/domain/post/dto/PostModifyRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/post/dto/PostModifyRequestDTO.java
@@ -1,5 +1,6 @@
 package com.hobbyhop.domain.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PostModifyRequestDTO {
 
+    @NotBlank
     @Size(min = 3, max = 50, message = "최소 3자 이상이여야 합니다.")
     private String postTitle;
 
+    @NotBlank
+    @Size(max = 500, message = "최대 500자 입니다.")
     private String postContent;
 }
--- src/main/java/com/hobbyhop/domain/post/dto/PostRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/post/dto/PostRequestDTO.java
@@ -1,6 +1,7 @@
 package com.hobbyhop.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,10 @@ import lombok.NoArgsConstructor;
 public class PostRequestDTO {
 
     @NotBlank
+    @Size(min = 3, max = 50, message = "최소 3자 이상이여야 합니다.")
     private String postTitle;
 
     @NotBlank
+    @Size(max = 500, message = "최대 500자 입니다.")
     private String postContent;
 }
