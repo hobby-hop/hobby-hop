33eb06ca908a2c850e8737fc1a7335522ac8553c
Gwon JunHyeok <jh10253267@gmail.com>
2024-02-01 00:06:12 +0900

    refactor(#277) : 불필요한 유효성 검증 애노테이션 제거

--- src/main/java/com/hobbyhop/domain/user/dto/UpdateProfileRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/user/dto/UpdateProfileRequestDTO.java
@@ -4,6 +4,7 @@ import com.hobbyhop.domain.user.dto.validCustom.NullablePattern;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,24 +12,10 @@ public class UpdateProfileRequestDTO {
     private String info;
 
     @NotBlank(message = "oldPassword : 프로필 수정 시 필수 입력 값입니다.")
-    @NullablePattern(regexp = "^[a-zA-Z0-9]+$",
-        msg1 = "oldPassword : 비밀번호는 알파벳 대소문자, 숫자의 조합으로 입력해야 합니다.",
-        min = 8,
-        max = 15,
-        msg2 = "oldPassword : 비밀번호는 8자리 이상, 15자리 이하로 입력해야 합니다.")
     private String oldPassword;
 
-    @NullablePattern(regexp = "^[a-zA-Z0-9]+$",
-        msg1 = "newPassword : 새 비밀번호는 알파벳 대소문자, 숫자의 조합으로 입력해야 합니다.",
-        min = 8,
-        max = 15,
-        msg2 = "newPassword : 새 비밀번호는 8자리 이상, 15자리 이하로 입력해야 합니다.")
+    @Size(min=8 , max=15)
     private String newPassword;
 
-    @NullablePattern(regexp = "^[a-zA-Z0-9]+$",
-        msg1 = "confirmPassword : 새 비밀번호는 알파벳 대소문자, 숫자의 조합으로 입력해야 합니다.",
-        min = 8,
-        max = 15,
-        msg2 = "confirmPassword : 새 비밀번호는 8자리 이상, 15자리 이하로 입력해야 합니다.")
     private String confirmPassword;
 }
