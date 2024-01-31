ef63288fdc24c8371e75cb1afb9bf6be2383593f
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-31 12:03:57 +0900

    feat(#265) : 관리자 권한 체크 로직 구현
    관리자인지 체크하는 쿼리 작성

--- src/main/java/com/hobbyhop/domain/clubmember/repository/custom/impl/ClubMemberRepositoryCustomImpl.java
+++ src/main/java/com/hobbyhop/domain/clubmember/repository/custom/impl/ClubMemberRepositoryCustomImpl.java
@@ -2,6 +2,7 @@ package com.hobbyhop.domain.clubmember.repository.custom.impl;
 
 import static com.hobbyhop.domain.clubmember.entity.QClubMember.clubMember;
 
+import com.hobbyhop.domain.clubmember.enums.MemberRole;
 import com.hobbyhop.domain.clubmember.repository.custom.ClubMemberRepositoryCustom;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class ClubMemberRepositoryCustomImpl implements ClubMemberRepositoryCusto
                 .from(clubMember)
                 .where(clubMember.clubMemberPK.club.id.eq(clubId).and(clubMember.clubMemberPK.user.id.eq(userId)))
                 .fetchFirst();
+        return i != null;
+    }
+
+    @Override
+    public boolean isAdminMember(Long clubId, Long userId) {
+        Integer i = jpaQueryFactory
+                .selectOne()
+                .from(clubMember)
+                .where(clubMember.clubMemberPK.club.id.eq(clubId).and(clubMember.clubMemberPK.user.id.eq(userId)).and(clubMember.memberRole.eq(MemberRole.ADMIN)))
+                .fetchFirst();
+
         return i != null;
     }
 }
