730ad1dff759bb0cebefd85034c7b25ce4891b80
Gwon JunHyeok <jh10253267@gmail.com>
2024-02-02 21:23:39 +0900

    refactor(#255) : ClubPageRequestDTO 작성
    컨트롤러단 파라미터 자동수집 기능을 사용하기 위해 getter/setter, 2NoArgsConstructor를 오버라이드함

--- src/main/java/com/hobbyhop/domain/club/dto/ClubPageRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/club/dto/ClubPageRequestDTO.java
@@ -1,10 +1,67 @@
 package com.hobbyhop.domain.club.dto;
 
 import com.hobbyhop.global.request.PageRequestDTO;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
-@Getter
+@NoArgsConstructor
 public class ClubPageRequestDTO extends PageRequestDTO {
+    public ClubPageRequestDTO(int page, int size, boolean isDesc) {
+        super(page, size, isDesc);
+    }
+
+    @Override
+    public int getPage() {
+        return super.getPage();
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public boolean isDesc() {
+        return super.isDesc();
+    }
+
+    @Override
+    public void setPage(int page) {
+        super.setPage(page);
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
+
+    @Override
+    public void setDesc(boolean isDesc) {
+        super.setDesc(isDesc);
+    }
+
+    @Override
+    public Pageable getPageable(String... props) {
+        return super.getPageable(props);
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public Long getCategory() {
+        return category;
+    }
+
+    public void setCategory(Long category) {
+        this.category = category;
+    }
+
     private String keyword;
     private Long category;
+
 }
