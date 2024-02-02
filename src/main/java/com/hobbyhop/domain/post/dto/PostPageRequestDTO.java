f847c3fa10b6614ce86af430b1a804b5adbe0d9a
Gwon JunHyeok <jh10253267@gmail.com>
2024-02-02 21:24:06 +0900

    refactor(#255) : PostPageRequestDTO작성
    컨트롤러단 파라미터 자동수집 기능을 사용하기 위해 getter/setter, 2NoArgsConstructor를 오버라이드함

--- src/main/java/com/hobbyhop/domain/post/dto/PostPageRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/post/dto/PostPageRequestDTO.java
@@ -1,9 +1,54 @@
 package com.hobbyhop.domain.post.dto;
 
 import com.hobbyhop.global.request.PageRequestDTO;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+
+@NoArgsConstructor
 public class PostPageRequestDTO extends PageRequestDTO {
+    public PostPageRequestDTO(int page, int size, boolean isDesc) {
+        super(page, size, isDesc);
+    }
+
+    @Override
+    public int getPage() {
+        return super.getPage();
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public boolean isDesc() {
+        return super.isDesc();
+    }
+
+    @Override
+    public void setPage(int page) {
+        super.setPage(page);
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
+
+    @Override
+    public void setDesc(boolean isDesc) {
+        super.setDesc(isDesc);
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
     private String keyword;
 }
