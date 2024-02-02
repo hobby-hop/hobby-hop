e7bdceb343d19b69dd848ec2978d7f65cfa50971
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-02-02 23:30:14 +0900

    refactor(#255) : CommentPageRequestDTO 작성
    이전의 작업과 동일하게 작업함.

--- src/main/java/com/hobbyhop/domain/comment/dto/CommentPageRequestDTO.java
+++ src/main/java/com/hobbyhop/domain/comment/dto/CommentPageRequestDTO.java
@@ -1,9 +1,59 @@
 package com.hobbyhop.domain.comment.dto;
 
 import com.hobbyhop.global.request.PageRequestDTO;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
-@Getter
+@NoArgsConstructor
 public class CommentPageRequestDTO extends PageRequestDTO {
+    public CommentPageRequestDTO(int page, int size, boolean isDesc) {
+        super(page, size, isDesc);
+    }
+
+    @Override
+    public int getPage() {
+        return super.getPage();
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public boolean isDesc() {
+        return super.isDesc();
+    }
+
+    @Override
+    public void setPage(int page) {
+        super.setPage(page);
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
+
+    @Override
+    public void setDesc(boolean isDesc) {
+        super.setDesc(isDesc);
+    }
+
+    @Override
+    public Pageable getPageable(String... props) {
+        return super.getPageable(props);
+    }
+
+    public String getStandard() {
+        return standard;
+    }
+
+    public void setStandard(String standard) {
+        this.standard = standard;
+    }
+
     private String standard;
 }
