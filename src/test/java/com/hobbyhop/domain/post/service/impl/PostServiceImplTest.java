a7cd99fff3d650ba17c1d92df93c04b6722f50ac
wkdehf217 <wkdehfdl17@naver.com>
2024-02-05 18:00:39 +0900

    feature/#294 post testcode

--- src/test/java/com/hobbyhop/domain/post/service/impl/PostServiceImplTest.java
+++ src/test/java/com/hobbyhop/domain/post/service/impl/PostServiceImplTest.java
@@ -1,28 +1,19 @@
 package com.hobbyhop.domain.post.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.IMAGE_JPEG;
 
-import com.hobbyhop.domain.category.entity.Category;
-import com.hobbyhop.domain.club.entity.Club;
 import com.hobbyhop.domain.club.service.impl.ClubServiceImpl;
-import com.hobbyhop.domain.clubmember.enums.MemberRole;
-import com.hobbyhop.domain.clubmember.service.ClubMemberService;
 import com.hobbyhop.domain.clubmember.service.impl.ClubMemberServiceImpl;
+import com.hobbyhop.domain.post.dto.PostModifyRequestDTO;
 import com.hobbyhop.domain.post.dto.PostRequestDTO;
 import com.hobbyhop.domain.post.dto.PostResponseDTO;
-import com.hobbyhop.domain.post.entity.Post;
 import com.hobbyhop.domain.post.repository.PostRepository;
 import com.hobbyhop.domain.post.s3.S3Service;
-import com.hobbyhop.domain.user.entity.User;
-import com.hobbyhop.domain.user.repository.UserRepository;
 import com.hobbyhop.test.CategoryTest;
 import com.hobbyhop.test.ClubTest;
 import com.hobbyhop.test.PostTest;
@@ -47,8 +38,6 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
     private PostServiceImpl postServiceImpl;
 
     @Mock
-    private UserRepository userRepository;
-    @Mock
     private PostRepository postRepository;
     @Mock
     private ClubServiceImpl clubServiceImpl;
@@ -56,75 +45,56 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
     private ClubMemberServiceImpl clubMemberService;
     @Mock
     private S3Service s3Service;
-    private Category category;
-    private Club club;
-    private Post post;
 
+    private PostRequestDTO postRequestDTO;
+    private PostResponseDTO postResponseDTO;
+    private PostModifyRequestDTO postModifyRequestDTO;
 
     @BeforeEach
     public void setup() {
 
-        category = Category.builder()
-                .id(TEST_CATEGORY_ID)
-                .categoryName(TEST_CATEGORY_NAME)
-                .description(TEST_CATEGORY_DESCRIPTION)
-                .build();
-
-        club = Club.builder()
-                .id(TEST_CLUB_ID)
-                .category(category)
-                .title(TEST_CLUB_TITLE)
-                .content(TEST_CLUB_CONTENT)
-                .build();
-
-        post = Post.builder()
-                .id(TEST_POST_ID)
+        postRequestDTO = PostRequestDTO.builder()
                 .postTitle(TEST_POST_TITLE)
                 .postContent(TEST_POST_CONTENT)
-                .user(TEST_USER)
-                .club(club)
+                .build();
+
+        postResponseDTO = PostResponseDTO.fromEntity(TEST_POST);
+
+        postModifyRequestDTO = PostModifyRequestDTO.builder()
+                .postTitle(TEST_POST_TITLE)
+                .postContent(TEST_POST_CONTENT)
                 .build();
     }
 
     @Test
     @DisplayName("게시글 생성 성공 테스트")
     void 게시글생성테스트() {
-//
-//        //given
-//        String testPostTitle = "testTitle";
-//        String testPostContent = "testContent";
-//        Long clubId = 1L;
-//        clubMemberService.joinClub(TEST_CLUB,TEST_USER);
-//
-//
-//        //when
-//        PostResponseDTO postResponseDTO = postServiceImpl.makePost(TEST_USER, clubId,
-//                PostRequestDTO.builder()
-//                        .postTitle(testPostTitle)
-//                        .postContent(testPostContent)
-//                        .build());
-//
-//        //then
-//        assertThat(testPostTitle).isEqualTo(postResponseDTO.getPostTitle());
-//
-//        assertThat(postServiceImpl.makePost(TEST_USER, clubId, PostRequestDTO.builder()
-//                .postContent(testPostContent)
-//                .postTitle(testPostTitle)
-//                .build())).isEqualTo(postResponseDTO);
+
+        // given
+        given(clubMemberService.isClubMember(TEST_CLUB_ID, TEST_USER_ID)).willReturn(true);
+        given(clubServiceImpl.findClub(TEST_CLUB_ID)).willReturn(TEST_CLUB);
+        given(postRepository.save(any())).willReturn(TEST_POST);
+
+        // when - then
+        assertThat(postServiceImpl.makePost(TEST_USER, TEST_CLUB_ID, postRequestDTO)
+                .getPostTitle()).isEqualTo(TEST_POST_TITLE);
+        assertThat(postServiceImpl.makePost(TEST_USER, TEST_CLUB_ID, postRequestDTO)
+                .getPostContent()).isEqualTo(TEST_POST_CONTENT);
+
     }
 
     @Test
     @DisplayName("이미지 업로드 성공 테스트")
     void 이미지업로드테스트() {
 
-        //given
+        // given
         String originalImageUrl = "testUrl";
         String savedImageUrl = "testSavedUrl";
 
-        //when
+        // when
         TEST_POST.changeImageUrl(originalImageUrl, savedImageUrl);
 
-        //then
+        // then
         assertThat(TEST_POST.getOriginImageUrl()).isEqualTo(originalImageUrl);
         assertThat(TEST_POST.getSavedImageUrl()).isEqualTo(savedImageUrl);
     }
@@ -133,62 +103,56 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
     @DisplayName("게시글 단건 조회 성공 테스트")
     void 게시글단건조회테스트() {
 
-//        //given
-//        when(clubServiceImpl.findClub(any())).thenReturn(club);
-//        when(postRepository.findById(any())).thenReturn(Optional.ofNullable(post));
-//        clubMemberService.joinClub(TEST_CLUB,TEST_USER, MemberRole.valueOf("MEMBER"));
-//
-//        //when
-//        PostResponseDTO postResponseDTO = postServiceImpl.getPostById(TEST_USER, TEST_CLUB_ID, TEST_POST_ID);
-//
-//        //then
-//        assertThat(postResponseDTO.getPostId()).isEqualTo(TEST_POST_ID);
+        // given
+        given(postRepository.findById(TEST_POST_ID)).willReturn(Optional.of(TEST_POST));
+
+        // when - then
+        assertThat(postServiceImpl.findPost(TEST_POST_ID).getId()).isEqualTo(
+                postResponseDTO.getPostId());
+        assertThat(postServiceImpl.findPost(TEST_POST_ID).getPostTitle()).isEqualTo(
+                postResponseDTO.getPostTitle());
+        assertThat(postServiceImpl.findPost(TEST_POST_ID).getPostContent()).isEqualTo(
+                postResponseDTO.getPostContent());
     }
 
     @Test
     @DisplayName("게시글 수정 성공 테스트")
-    // 파일을 넣어주는 경로가 로컬마다 다르기때문에 주석처리.
     void 게시글수정테스트() throws IOException {
 
-        //given
-//        String testPostTitle = "test";
-//        String testPostContent = "test";
-//
-//        String fileUrl = "file:///C:/Users/wkdeh/OneDrive/Pictures/images.IMG.jfif";
-//        Resource fileResource = new ClassPathResource(fileUrl);
-//        MockMultipartFile multipartFile =
-//                new MockMultipartFile(
-//                        "image1",
-//                        fileResource.getFilename(),
-//                        IMAGE_JPEG.getType(),
-//                        fileResource.getInputStream());
-//
-//        //when
-//        PostResponseDTO postResponseDTO = postServiceImpl.modifyPost(TEST_OTHER_USER,TEST_OTHER_CLUB_ID,TEST_OTHER_POST_ID,multipartFile,
-//                PostRequestDTO.builder()
-//                        .postTitle(testPostTitle)
-//                        .postContent(testPostContent)
-//                        .build());
-//
-//        //then
-//        assertThat(postResponseDTO.getPostTitle()).isEqualTo(testPostTitle);
+        // given
+        given(clubMemberService.isClubMember(TEST_CLUB_ID, TEST_USER_ID)).willReturn(true);
+        given(clubServiceImpl.findClub(TEST_CLUB_ID)).willReturn(TEST_CLUB);
+        given(postRepository.findById(TEST_POST_ID)).willReturn(Optional.of(TEST_POST));
+
+        String fileUrl = "images/image1.jpg";
+        Resource fileResource = new ClassPathResource(fileUrl);
+        MockMultipartFile multipartFile =
+                new MockMultipartFile(
+                        "image1",
+                        fileResource.getFilename(),
+                        IMAGE_JPEG.getType(),
+                        fileResource.getInputStream());
+
+        // when - then
+        assertThat(postServiceImpl.modifyPost(TEST_USER, TEST_CLUB_ID, TEST_POST_ID, multipartFile,
+                postModifyRequestDTO).getPostTitle()).isEqualTo(postResponseDTO.getPostTitle());
+
     }
 
     @Test
     @DisplayName("게시글 삭제 성공 테스트")
     void 게시글삭제테스트() {
-//
-//        //given
-//        given(postRepository.findById(any())).willReturn(Optional.of(TEST_POST));
-//        given(clubServiceImpl.findClub(any())).willReturn(TEST_CLUB);
-//        given(postRepository.save(any())).willReturn(TEST_OTHER_POST);
-//        clubMemberService.joinClub(TEST_CLUB,TEST_USER);
-//
-//        //when
-//        postRepository.save(post);
-//        postServiceImpl.deletePost(TEST_USER, TEST_CLUB_ID, post.getId());
-//
-//        //then
-//        verify(postRepository, times(1)).deleteById(TEST_POST_ID);
+
+        // given
+        given(clubMemberService.isClubMember(TEST_CLUB_ID, TEST_USER_ID)).willReturn(true);
+        given(clubServiceImpl.findClub(TEST_CLUB_ID)).willReturn(TEST_CLUB);
+        given(postRepository.findById(TEST_POST_ID)).willReturn(Optional.of(TEST_POST));
+
+        // when
+        postServiceImpl.deletePost(TEST_USER, TEST_CLUB_ID, TEST_POST_ID);
+
+        // then
+        verify(postRepository, times(1)).deleteAllElement(TEST_POST_ID);
     }
 }
+
