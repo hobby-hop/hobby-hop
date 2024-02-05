bfad513fda023f92eaa853ee488b4793cea35238
Gwon JunHyeok <jh10253267@gmail.com>
2024-02-05 19:16:14 +0900

    refactor(#291) : ClubMember Custom 메소드 작성

--- src/main/java/com/hobbyhop/domain/clubmember/repository/custom/ClubMemberRepositoryCustom.java
+++ src/main/java/com/hobbyhop/domain/clubmember/repository/custom/ClubMemberRepositoryCustom.java
@@ -3,4 +3,5 @@ package com.hobbyhop.domain.clubmember.repository.custom;
 public interface ClubMemberRepositoryCustom {
     boolean isClubMember(Long clubId, Long userId);
     boolean isAdminMember(Long clubId, Long userId);
+    boolean isMemberLimitReached(Long userId);
 }
