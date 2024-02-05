71244b2846f4a2a4093fd7e92c86503f48834104
wkdehf217 <wkdehfdl17@naver.com>
2024-02-05 19:52:10 +0900

    feature/#294 post testcode

--- src/test/java/com/hobbyhop/domain/post/service/impl/PostServiceImplTest.java
+++ src/test/java/com/hobbyhop/domain/post/service/impl/PostServiceImplTest.java
@@ -10,15 +10,21 @@ import static org.springframework.http.MediaType.IMAGE_JPEG;
 import com.hobbyhop.domain.club.service.impl.ClubServiceImpl;
 import com.hobbyhop.domain.clubmember.service.impl.ClubMemberServiceImpl;
 import com.hobbyhop.domain.post.dto.PostModifyRequestDTO;
+import com.hobbyhop.domain.post.dto.PostPageRequestDTO;
+import com.hobbyhop.domain.post.dto.PostPageResponseDTO;
 import com.hobbyhop.domain.post.dto.PostRequestDTO;
 import com.hobbyhop.domain.post.dto.PostResponseDTO;
 import com.hobbyhop.domain.post.repository.PostRepository;
 import com.hobbyhop.domain.post.s3.S3Service;
+import com.hobbyhop.domain.postuser.repository.PostUserRepository;
+import com.hobbyhop.domain.postuser.service.impl.PostUserServiceImpl;
 import com.hobbyhop.test.CategoryTest;
 import com.hobbyhop.test.ClubTest;
 import com.hobbyhop.test.PostTest;
 import com.hobbyhop.test.UserTest;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,6 +35,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.mock.web.MockMultipartFile;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,6 +47,10 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
     @Mock
     private PostRepository postRepository;
     @Mock
+    private PostUserServiceImpl postUserService;
+    @Mock
+    private PostUserRepository postUserRepository;
+    @Mock
     private ClubServiceImpl clubServiceImpl;
     @Mock
     private ClubMemberServiceImpl clubMemberService;
@@ -49,6 +60,7 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
     private PostRequestDTO postRequestDTO;
     private PostResponseDTO postResponseDTO;
     private PostModifyRequestDTO postModifyRequestDTO;
+    private PostPageRequestDTO postPageRequestDTO;
 
     @BeforeEach
     public void setup() {
@@ -64,6 +76,8 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
                 .postTitle(TEST_POST_TITLE)
                 .postContent(TEST_POST_CONTENT)
                 .build();
+
+        postPageRequestDTO = new PostPageRequestDTO(1, 1, true);
     }
 
     @Test
@@ -153,6 +167,44 @@ class PostServiceImplTest implements PostTest, UserTest, CategoryTest, ClubTest 
 
         // then
         verify(postRepository, times(1)).deleteAllElement(TEST_POST_ID);
+    }
+
+    @Test
+    @DisplayName("게시글 좋아요 성공 테스트")
+    void 게시글좋아요테스트() {
+
+        // given
+        given(clubServiceImpl.findClub(TEST_CLUB_ID)).willReturn(TEST_CLUB);
+        given(postRepository.findById(TEST_POST_ID)).willReturn(Optional.of(TEST_POST));
+
+        // when
+        postServiceImpl.makePostUser(TEST_USER, TEST_CLUB_ID, TEST_POST_ID);
+
+        // then
+        assertThat(TEST_POST.getLikeCnt().equals(1L));
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회 성공 테스트")
+    void 게시글전체조회테스트() {
+
+        List<PostPageResponseDTO> content = new ArrayList<>();
+        PostPageResponseDTO postPageResponseDTO1 = new PostPageResponseDTO(TEST_CLUB_ID,
+                TEST_POST_ID, TEST_USER_NAME,
+                TEST_POST_TITLE, TEST_POST_NUMBER, TEST_POST_LIKE, TEST_CREATED_AT,
+                TEST_MODIFIED_AT);
+
+        content.add(postPageResponseDTO1);
+
+        // given
+        given(postRepository.findAllByClubId(postPageRequestDTO.getPageable("id"), TEST_CLUB_ID,
+                postPageRequestDTO.getKeyword())).willReturn(
+                new PageImpl<>(content, postPageRequestDTO.getPageable("id"), 1L));
+
+        // when - then
+        assertThat(
+                postServiceImpl.getAllPost(postPageRequestDTO, TEST_CLUB_ID).getSize()).isEqualTo(
+                1);
     }
 }
 
