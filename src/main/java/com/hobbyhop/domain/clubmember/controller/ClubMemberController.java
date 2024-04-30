604adab78a0c9c854c5be7d568075d80a5f598ec
Gwon JunHyeok <jh10253267@gmail.com>
2024-04-30 19:29:52 +0900

    fix(#319) : ClubMember에서 탈퇴 구현
    기존 club에서 받아서 clubMember에서 처리하고있었는데 club에서 트랜잭션 애노테이션을 붙여주지 않아서 탈퇴되지 않는 문제 해결.

--- src/main/java/com/hobbyhop/domain/clubmember/controller/ClubMemberController.java
+++ src/main/java/com/hobbyhop/domain/clubmember/controller/ClubMemberController.java
@@ -8,27 +8,32 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/clubs/{clubId}")
-@SecurityRequirement(name = "Bearer Authentication")
 public class ClubMemberController {
     private final ClubMemberService clubMemberService;
 
     @Operation(summary = "모임의 관리자인지 체크")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/checkPermission")
     public ApiResponse<?> checkPermission(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable("clubId") Long clubId) {
         return ApiResponse.ok(clubMemberService.isAdminMember(clubId, userDetails.getUser().getId()));
     }
 
     @Operation(summary = "모임의 멤버인지 체크")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/checkClubMember")
     public ApiResponse<?> checkClubMember(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable("clubId") Long clubId) {
         return ApiResponse.ok(clubMemberService.isClubMember(clubId, userDetails.getUser().getId()));
+    }
+    @Operation(summary = "모임 탈퇴")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @DeleteMapping("/clubmembers/{userId}")
+    public ApiResponse<?> leaveMember(@PathVariable("clubId") Long clubId, @PathVariable("userId") Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        clubMemberService.leaveMember(clubId, userDetails.getUser(), userId);
+        return ApiResponse.ok("처리 완료");
     }
 }
