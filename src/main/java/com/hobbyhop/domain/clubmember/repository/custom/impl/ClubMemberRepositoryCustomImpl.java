24901d3bc0494dde78afd073f7521bf4d3525bab
Gwon JunHyeok <jh10253267@gmail.com>
2024-08-17 20:18:57 +0900

    reafctor(#341) : Fetchjoint삭제 및 쿼리 최적화
    대부분의 clubMember의 쓰임이 단순 ClubMember의 정보를 조회하는 것으로 연관된 엔티티를 무조건 로드하는 것은 불필요하다고 판단함

--- src/main/java/com/hobbyhop/domain/clubmember/repository/custom/impl/ClubMemberRepositoryCustomImpl.java
+++ src/main/java/com/hobbyhop/domain/clubmember/repository/custom/impl/ClubMemberRepositoryCustomImpl.java
@@ -1,30 +1,52 @@
 package com.hobbyhop.domain.clubmember.repository.custom.impl;
 
+import static com.hobbyhop.domain.club.entity.QClub.club;
 import static com.hobbyhop.domain.clubmember.entity.QClubMember.clubMember;
 
+import com.hobbyhop.domain.club.dto.ClubResponseDTO;
 import com.hobbyhop.domain.clubmember.entity.ClubMember;
 import com.hobbyhop.domain.clubmember.enums.MemberRole;
 import com.hobbyhop.domain.clubmember.repository.custom.ClubMemberRepositoryCustom;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 public class ClubMemberRepositoryCustomImpl implements ClubMemberRepositoryCustom {
-
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public Optional<ClubMember> findClubMember(Long clubId, Long userId) {
         return Optional.ofNullable(jpaQueryFactory.selectFrom(clubMember)
-                .join(clubMember.clubMemberPK.club).fetchJoin()
-                .join(clubMember.clubMemberPK.user).fetchJoin()
                 .where(clubMember.clubMemberPK.club.id.eq(clubId)
                         .and(clubMember.clubMemberPK.user.id.eq(userId)))
                 .fetchOne()
         );
     }
+
+    @Override
+    public List<ClubResponseDTO> findClubsByUserId(Long userId) {
+        return jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                ClubResponseDTO.class,
+                                club.id,
+                                club.title,
+                                club.content,
+                                club.category.id.as("categoryId"),
+                                club.category.categoryName,
+                                club.createdAt,
+                                club.modifiedAt
+                        ))
+                .from(clubMember)
+                .join(clubMember.clubMemberPK.club)
+                .where(clubMember.clubMemberPK.user.id.eq(userId))
+                .fetch();
+    }
+
 
     @Override
     public boolean isClubMember(Long clubId, Long userId) {
@@ -48,17 +70,14 @@ public class ClubMemberRepositoryCustomImpl implements ClubMemberRepositoryCusto
     }
 
     @Override
-    public boolean isMemberLimitReached(Long userId) {
+    public boolean isClubLimitReached(Long userId) {
         int limitCount = 9;
-        long i = jpaQueryFactory
-                .selectOne()
+        long currentMyClubCount = jpaQueryFactory
+                .select(clubMember.count())
                 .from(clubMember)
                 .where(clubMember.clubMemberPK.user.id.eq(userId))
-                .fetchCount();
+                .fetchOne();
 
-        if (i >= limitCount) {
-            return true;
-        }
-        return false;
+        return currentMyClubCount >= limitCount;
     }
 }
